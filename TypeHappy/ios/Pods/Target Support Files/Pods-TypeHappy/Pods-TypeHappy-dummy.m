479cd2277422fb70d67db43f7a940adacf2423bc
Mayank Jethva <m.jethva01@gmail.com>
2017-07-02 20:19:52 -0700

    initial commit

---
+++ TypeHappy/ios/Pods/Target Support Files/Pods-TypeHappy/Pods-TypeHappy-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TypeHappy : NSObject
+@end
+@implementation PodsDummy_Pods_TypeHappy
+@end
---
+++ TypeHappy/ios/Pods/Target Support Files/Pods-TypeHappyKeyboard/Pods-TypeHappyKeyboard-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TypeHappyKeyboard : NSObject
+@end
+@implementation PodsDummy_Pods_TypeHappyKeyboard
+@end
